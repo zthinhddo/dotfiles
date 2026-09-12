3770c22676e3ae54c73599a4b93a7cabf5274d08
zthinhddo <ddthinh.dev@gmail.com>
2026-09-12 14:40:34 +0700

    (neovim) cleanup

--- nvim/lua/plugins/ai/copilot.lua
+++ nvim/lua/plugins/ai/copilot.lua
@@ -1,35 +1,33 @@
 return {
   "zbirenbaum/copilot.lua",
-  lazy = true,
+  lazy = true, --  Copilot server takes long time to start
   cmd = "Copilot",
-  event = "InsertEnter",
-  opts = function()
-    require("copilot").setup({
-      filetypes = {
-        yaml = true,
-        markdown = true,
-        gitcommit = true,
-        gitrebase = false,
-        javascript = true,
-        typescript = true,
-        lua = true,
-        ["*"] = false,
+  -- @type copilot.Config
+  opts = {
+    filetypes = {
+      yaml = true,
+      markdown = true,
+      gitcommit = true,
+      gitrebase = false,
+      javascript = true,
+      typescript = true,
+      lua = true,
+      ["*"] = false,
+    },
+    suggestion = {
+      enabled = false,
+      auto_trigger = true,
+      hide_during_completion = true,
+      debounce = 75,
+      trigger_on_accept = true,
+      keymap = {
+        accept = "<M-l>",
+        accept_word = false,
+        accept_line = false,
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
       },
-      suggestion = {
-        enabled = false,
-        auto_trigger = true,
-        hide_during_completion = true,
-        debounce = 75,
-        trigger_on_accept = true,
-        keymap = {
-          accept = "<M-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
-        },
-      },
-    })
-  end,
+    },
+  },
 }
